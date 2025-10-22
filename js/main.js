bb73878d07b29f22a2022a9a69cb3870ae146282
Madhur Chandratre <madhurchandratre@gmail.com>
2025-10-22 12:34:39 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,41 +8,38 @@ $(document).ready(() => {
 function getMovies(searchText){
     axios.get(`/api/movies?title=${encodeURIComponent(searchText)}&type=search`)
     .then((response) => {
-        console.log(response)
-        let movies = response.data.Search;
+        const movies = response.data.Search;
         let output = '';
         $.each(movies, (index, movie) => {
             output += `
                 <div class="col-md-3">
                     <div class="well text-center">
-                        <img src="${movie.Poster}">
+                        <img src="${movie.Poster}" alt="${movie.Title}">
                         <h5>${movie.Title}</h5>
-                        <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">
-                            Movie Details
-                        </a>
+                        <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
                     </div>
                 </div>
             `;
         });
         $('#movies').html(output);
-    }).catch((err) =>{
-        console.log(err)
+    })
+    .catch((err) => {
+        console.log(err);
+        $('#movies').html('<p class="text-danger">No movies found or error occurred.</p>');
     });
 }
+
 function movieSelected(id){
     sessionStorage.setItem('movieId', id);
     window.location = 'movie.html';
     return false;
 }
 
-function getMovie()
-{
+function getMovie() {
     let movieId = sessionStorage.getItem('movieId'); 
-    axios.get(`/api/movies?title=${encodeURIComponent(movieId)}`)
+    axios.get(`/api/movies?title=${encodeURIComponent(movieId)}&type=id`)
     .then((response) => {
-        console.log(response);
         let movie = response.data;
-
         let output = `
             <div class="row p-3 border border-primary rounded-4 my-4">
                 <div class="col-md-4 text-center">
@@ -51,27 +48,13 @@ function getMovie()
                 <div class="col-md-8">
                     <h2>${movie.Title}</h2>
                     <ul class="list-group">
-                        <li class="list-group-item"><strong>Genre: </strong>
-                            ${movie.Genre}
-                        </li>
-                        <li class="list-group-item"><strong>Release: </strong>
-                            ${movie.Released}
-                        </li>
-                        <li class="list-group-item"><strong>Rated: </strong>
-                            ${movie.Rated}
-                        </li>
-                        <li class="list-group-item"><strong>IMDB Rating: </strong>
-                            ${movie.imdbRating}
-                        </li>
-                        <li class="list-group-item"><strong>Director: </strong>
-                            ${movie.Director}
-                        </li>
-                        <li class="list-group-item"><strong>Writer: </strong>
-                            ${movie.Writer}
-                        </li>
-                        <li class="list-group-item"><strong>Actors: </strong>
-                            ${movie.Actors}
-                        </li>
+                        <li class="list-group-item"><strong>Genre: </strong>${movie.Genre}</li>
+                        <li class="list-group-item"><strong>Release: </strong>${movie.Released}</li>
+                        <li class="list-group-item"><strong>Rated: </strong>${movie.Rated}</li>
+                        <li class="list-group-item"><strong>IMDB Rating: </strong>${movie.imdbRating}</li>
+                        <li class="list-group-item"><strong>Director: </strong>${movie.Director}</li>
+                        <li class="list-group-item"><strong>Writer: </strong>${movie.Writer}</li>
+                        <li class="list-group-item"><strong>Actors: </strong>${movie.Actors}</li>
                     </ul>
                 </div>
             </div>
@@ -80,13 +63,16 @@ function getMovie()
                     <h3>Plot</h3>
                     ${movie.Plot}
                     <hr>
-                    <a href="https://imdb.com/title/${movie.imdbID}" target="_blank"
-                    class="btn btn-primary">View IMDB</a>
+                    <a href="https://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
                     <a href="index.html" class="btn btn-default">Go Back To Search</a>
+                </div>
+            </div>
         `;
-
-        $('#movie').html(output)
-    }).catch((err) =>{
-        console.log(err)
+        $('#movie').html(output);
+    })
+    .catch((err) => {
+        console.log(err);
+        $('#movie').html('<p class="text-danger">Movie details not found or error occurred.</p>');
     });
 }
+
